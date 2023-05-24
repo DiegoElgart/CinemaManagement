d22fd75ad9b5eaa1b0794b079bc7b25ec18bd4fb
DiegoElgart <diegojelgart@gmail.com>
2023-05-24 17:03:57 +0300

    Figuring out Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import rootReducer from "./reducers/rootReducer";
-import { BrowserRouter } from "react-router-dom";
-
-const store = configureStore({ reducer: rootReducer });
+const store = "";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
